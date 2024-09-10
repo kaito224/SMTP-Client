ea5590686c98824ad16f06ff36adbb3a35d02667
david <me@mail.org>
2024-09-10 12:43:38 +0200

    added attachments/mime, verbosity and checks for  some responses

--- client.py
+++ client.py
@@ -2,6 +2,11 @@ from socket import * # type: ignore
 from commands import *
 import argparse
 import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from typing import List
 
 
 def authenticate(socket: socket, username:str, password, authentication_method = "LOGIN"):
@@ -14,6 +19,29 @@ def authenticate(socket: socket, username:str, password, authentication_method =
         return authPlain_CMD(socket, username, password)
     
     raise Exception("authentication method did not match implemented methods")
+
+
+def create_mime(sender: str, recipients, subject: str, textpayload: str, attachment_paths: List[str]) -> str:
+    #Header
+    message = MIMEMultipart()
+    message["From"] = sender
+    message["To"] = ", ".join(recipients)
+    message["Subject"] = subject
+    #Payload
+    message.attach(MIMEText(textpayload))
+    if not attachment_paths:                 #Just return if there are no attachments
+        return message.as_string()
+    
+    #attach files
+    for path in attachment_paths:
+        with open(path, "rb") as f:
+            data = f.read()
+            attachment = MIMEBase("application", "octet-stream")
+            attachment.set_payload(data)
+            encoders.encode_base64(attachment)
+            attachment.add_header("Content-Disposition", f'attachment; filename="{path.split("/")[-1]}"')
+            message.attach(payload=attachment)
+    return message.as_string()
 
 
 def main():
@@ -30,11 +58,13 @@ def main():
     SUBJECT = " ".join(args.subject) if args.subject is not None else ""
     TXTMESSAGE = " ".join(args.text) if args.text is not None else ""
     AUTHENTICATION_METHOD = args.authentication_method
+    ATTACHMENT_PATHS = args.attachments if args.attachments is not None else []
+    VERBOSE = args.v
 
     #open socket and create a tcp connection
     with socket(AF_INET, SOCK_STREAM) as clientSocket:
         
-        #connect to mailserver and wait for innit message
+        #connect to mailswerver and wait for innit message
         clientSocket.connect(MAILSERVER)
         server_message_innit = clientSocket.recv(1024).decode()
         if server_message_innit[:3] != '220':
@@ -44,11 +74,11 @@ def main():
 
         #start ssl
         if not args.no_ssl:
-            (sslClientSocket, server_message) = starttls_CDM(clientSocket, MAILSERVER[0])
+            (sslClientSocket, server_message_ssl) = starttls_CDM(clientSocket, MAILSERVER[0])
             if sslClientSocket is None:
-                print(f"""Could not establish tls connection got error: {server_message}""")
+                print(f"""Could not establish tls connection got error: {server_message_ssl}""")
                 return
-            print("SSL connection has been successfully established")
+            if VERBOSE: print("SSL connection has been successfully established")
             helo_CMD(sslClientSocket, FQDN)# I dont know why but you have to send a helo after establishing a ssl/tls connection
             clientSocket = sslClientSocket
         
@@ -74,22 +104,36 @@ def main():
                 return
             if PASSWORD is None:
                 PASSWORD = getpass.getpass("Password: ")
+
             server_message_authentication = authenticate(clientSocket, USERNAME, PASSWORD, AUTHENTICATION_METHOD)
             if server_message_authentication[:3] != "235" : 
-                print(f"""Could not authenticate got server error: {server_message_authentication}""")
+                print(f"""Could not authenticate. Got server error: {server_message_authentication}""")
                 return
             
             #mail
-            #TODO : Check responses
-            mail_CMD(clientSocket, SENDER)
+            server_message_mail = mail_CMD(clientSocket, SENDER)
+            if server_message_mail[:3] != "250":
+                print(f"""Mail command failed. Got server error: {server_message_mail}""")
+                return
+            
             for recipient in RECIPIENTS:
-                rcpt_CMD(clientSocket, recipient)
+                server_message_rcpt = rcpt_CMD(clientSocket, recipient)
+                if server_message_rcpt[:3] != "250":
+                    print(f"""RCPT command failed for {recipient}. Got server error: {server_message_rcpt}""")
+                    return
 
             #data
-            #TODO : message constructor which adds each recipient and allows for attachments
-            message = message=f"""From: {SENDER}\r\nTo: {RECIPIENTS[0]}\r\nSubject: {SUBJECT}\r\n\r\n{TXTMESSAGE}"""
+            if args.no_mime:
+                message = TXTMESSAGE
+            else:   
+                message = create_mime(SENDER,RECIPIENTS,SUBJECT,TXTMESSAGE,ATTACHMENT_PATHS)
             server_message_data = data_CMD(clientSocket,message)
-            print(server_message_data)
+            if server_message_data[:3] != "250":
+                    print(f"""DATA command failed. Got server error: {server_message_data}""")
+                    return
+            print(f"""Email was send. Received Server message: {server_message_data}""")
+
+            #quit
             quit_CMD(clientSocket)
 
             clientSocket.close()
@@ -111,6 +155,14 @@ if __name__ == "__main__":
     
     parser.add_argument("-no-ssl",
                         help="Do not use ssl/tls when connecting to a mailserver", 
+                        action="store_true")
+    
+    parser.add_argument("-no-mime",
+                        help="Do not use MIME when sending a mail. Subject and attachments will be disregarded", 
+                        action="store_true")
+    
+    parser.add_argument("-v",
+                        help="Make output more verbose", 
                         action="store_true")
     
     parser.add_argument("--port",
@@ -149,6 +201,10 @@ if __name__ == "__main__":
                         choices=["LOGIN", "PLAIN"],
                         default = "LOGIN",
                         help="Chose a SASL method for authentication. Login is set as default")
+    
+    parser.add_argument("--attachments",
+                        nargs="+",
+                        help="Paths to file attachments")
 
 
     #call main
